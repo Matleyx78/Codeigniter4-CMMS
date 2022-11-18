f0f5ad076ed007f611d0d4842eca105aa385d519
Matleyx78 <matmat78@hotmail.it>
2022-11-18 07:32:36 +0100

    Mod file

--- src/Database/Migrations/2020-12-28-223112_create_cmms_tables.php
+++ src/Database/Migrations/2020-12-28-223112_create_cmms_tables.php
@@ -9,41 +9,33 @@ use CodeIgniter\Database\Migration;
 class CreateCmmsTables extends Migration
 {
     private $prefix_table = 'cmms_';
-    private $tab_buildings = array('tname' => 'worksite', 'pr' => 'wosi',);
-    private $tab_sectors = array('tname' => 'impianti', 'pr' => 'impi',);
-    private $tab_assets = array('tname' => 'macchine', 'pr' => 'macc',);
+    private $tab_buildings = array('tname' => 'worksite', 'pr' => 'wosi', );
+    private $tab_sectors = array('tname' => 'impianti', 'pr' => 'impi', );
+    private $tab_assets = array('tname' => 'macchine', 'pr' => 'macc', );
+    private $tab_counters = array('tname' => 'contatori', 'pr' => 'cont', );
 
     public function up(): void
     {
 
-        //$prefix_table = 'cmms_';
-        //$tab_buildings_name = array('tname' => 'worksite', 'pr' => 'wosi',);
-        //$tab_sectors_name = array('tname' => 'impianti', 'pr' => 'impi',);
-        //$tab_assets_name = array('tname' => 'macchine', 'pr' => 'macc',);
-        $tab_immediate_jobs_name = array('tname' => 'interventi_immediati', 'pr' => 'imjo',);
-        $tab_scheduled_jobs_name = array('tname' => 'interventi_programmati', 'pr' => 'scjo',);
-        $tab_executed_jobs_name = array('tname' => 'interventi_eseguiti', 'pr' => 'exjo',);
-
-
         // Buildings
         $this->forge->addField([
-            'id_' . $this->tab_buildings['pr']             => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            $this->tab_buildings['pr'] . '_description'    => ['type' => 'varchar', 'constraint' => 150],
-            $this->tab_buildings['pr'] . '_created_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_buildings['pr'] . '_updated_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_buildings['pr'] . '_deleted_at'     => ['type' => 'datetime', 'null' => true],
+            'id_' . $this->tab_buildings['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            $this->tab_buildings['pr'] . '_description' => ['type' => 'varchar', 'constraint' => 150],
+            $this->tab_buildings['pr'] . '_created_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_buildings['pr'] . '_updated_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_buildings['pr'] . '_deleted_at' => ['type' => 'datetime', 'null' => true],
         ]);
         $this->forge->addPrimaryKey('id_' . $this->tab_buildings['pr']);
         $this->forge->createTable($this->prefix_table . '' . $this->tab_buildings['tname']);
 
         // Sectors
         $this->forge->addField([
-            'id_' . $this->tab_sectors['pr']           => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            $this->tab_sectors['pr'] . '_description'         => ['type' => 'varchar', 'constraint' => 150],
-            $this->tab_sectors['pr'] . '_id_' . $this->tab_buildings['pr']         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
-            $this->tab_sectors['pr'] . '_created_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_sectors['pr'] . '_updated_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_sectors['pr'] . '_deleted_at'     => ['type' => 'datetime', 'null' => true],
+            'id_' . $this->tab_sectors['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            $this->tab_sectors['pr'] . '_description' => ['type' => 'varchar', 'constraint' => 150],
+            $this->tab_sectors['pr'] . '_id_' . $this->tab_buildings['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
+            $this->tab_sectors['pr'] . '_created_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_sectors['pr'] . '_updated_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_sectors['pr'] . '_deleted_at' => ['type' => 'datetime', 'null' => true],
         ]);
         $this->forge->addPrimaryKey('id_' . $this->tab_sectors['pr']);
         $this->forge->addKey($this->tab_sectors['pr'] . '_id_' . $this->tab_buildings['pr']);
@@ -52,18 +44,28 @@ class CreateCmmsTables extends Migration
 
         // Assets
         $this->forge->addField([
-            'id_' . $this->tab_assets['pr']           => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            $this->tab_assets['pr'] . '_description'         => ['type' => 'varchar', 'constraint' => 150],
-            $this->tab_assets['pr'] . '_id_' . $this->tab_sectors['pr']         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
-            $this->tab_assets['pr'] . '_created_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_assets['pr'] . '_updated_at'     => ['type' => 'datetime', 'null' => true],
-            $this->tab_assets['pr'] . '_deleted_at'     => ['type' => 'datetime', 'null' => true],
+            'id_' . $this->tab_assets['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            $this->tab_assets['pr'] . '_description' => ['type' => 'varchar', 'constraint' => 150],
+            $this->tab_assets['pr'] . '_id_' . $this->tab_sectors['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
+            $this->tab_assets['pr'] . '_created_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_assets['pr'] . '_updated_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_assets['pr'] . '_deleted_at' => ['type' => 'datetime', 'null' => true],
         ]);
         $this->forge->addPrimaryKey('id_' . $this->tab_assets['pr']);
         $this->forge->addKey($this->tab_assets['pr'] . '_id_' . $this->tab_sectors['pr']);
         $this->forge->addForeignKey($this->tab_assets['pr'] . '_id_' . $this->tab_sectors['pr'], $this->prefix_table . '' . $this->tab_sectors['tname'], 'id_' . $this->tab_sectors['pr'], '', 'CASCADE');
         $this->forge->createTable($this->prefix_table . '' . $this->tab_assets['tname']);
 
+        // Counterss
+        $this->forge->addField([
+            'id_' . $this->tab_counters['pr'] => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            $this->tab_counters['pr'] . '_description' => ['type' => 'varchar', 'constraint' => 150],
+            $this->tab_counters['pr'] . '_created_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_counters['pr'] . '_updated_at' => ['type' => 'datetime', 'null' => true],
+            $this->tab_counters['pr'] . '_deleted_at' => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id_' . $this->tab_counters['pr']);
+        $this->forge->createTable($this->prefix_table . '' . $this->tab_counters['tname']);
     }
 
     // --------------------------------------------------------------------
